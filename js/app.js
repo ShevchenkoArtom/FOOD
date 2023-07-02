db15b609a4b0c0a7654371e0acafcd0946563c2f
Artom Shevchenko <87131438+ShevchenkoArtom@users.noreply.github.com>
2023-07-02 20:24:11 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+    const body = document.querySelector(".body");
+    const header = document.querySelector(".header__menu");
+    let book = document.getElementById("bookF");
+    let closeBtnPopUp = document.querySelector(".btn-close");
+    let btnsBook = document.querySelector(".bookBtn");
+    const popUp1 = document.querySelector(".popup-win1");
+    
+    btnsBook.addEventListener("click", function(){
+        popUp1.classList.toggle("dn");
+    });
 
+    closeBtnPopUp.addEventListener("click", function(){
+        popUp1.classList.toggle("dn")
+    });
+
+    book.addEventListener("click", function(){
+        alert("Вы забронировали столик. (На самом деле это просто макет, поэтому большая часть функционала не работет.)")
+    });
 });
